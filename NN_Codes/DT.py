79dde4e770ce1c9962992c0c8918638becd25153
Ruchi Jain <jain-ruchi@users.noreply.github.com>
2017-06-04 17:26:59 -0700

    Tune hyperparameters for kNN, decision trees, and random forest

--- NN_Codes/DT.py
+++ NN_Codes/DT.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 from parse_dataset import Crawler
 from sklearn.tree import DecisionTreeClassifier
@@ -8,10 +9,10 @@ OUTPUTS = 3
 TEST_SIZE = 200
 FEATURES = 28
 
-CRITERION = 'entropy'
-MAX_FEATURES = 'auto'
+CRITERION = 'gini'
+MAX_FEATURES = None
 MAX_DEPTH = 10
-MIN_SAMPLES_SPLIT = 50
+MIN_SAMPLES_SPLIT = 100
 
 
 objCrawler = Crawler("FinalCancer_Data.csv")
@@ -26,6 +27,10 @@ train_label = data[TEST_SIZE:, -OUTPUTS:]
 # normalize dataset
 norm = Normalizer()
 X = norm.fit_transform(train_data)
+Y = norm.transform(test_data)
+
+# ranking_losses = []
+# for i in range(9,31):
 
 # train classifier
 clf = DecisionTreeClassifier(criterion=CRITERION,
@@ -34,7 +39,6 @@ clf = DecisionTreeClassifier(criterion=CRITERION,
     min_samples_split=MIN_SAMPLES_SPLIT).fit(X, train_label)
 
 # make predictions on test data
-Y = norm.transform(test_data)
 predicted = clf.predict_proba(Y)
 scores = np.reshape(np.concatenate((predicted[0][:,1], predicted[1][:,1], predicted[2][:,1])), (TEST_SIZE, OUTPUTS))
 
@@ -43,14 +47,18 @@ scores = np.reshape(np.concatenate((predicted[0][:,1], predicted[1][:,1], predic
 # the best value is equal to the avg number of labels in test_label per sample
 # i.e. 2.0550000000000002
 cover_err = coverage_error(test_label, scores)
-print "Coverage error: " + str(cover_err)
+print "DecisionTree - Coverage error: " + str(cover_err)
 
 # label ranking average precision score
 # best value is 1
 lrap_score = label_ranking_average_precision_score(test_label, scores)
-print "Label ranking avg precision score: " + str(lrap_score)
+print "DecisionTree - Label ranking avg precision score: " + str(lrap_score)
 
 # compute label ranking loss
 # best value is 0
 ranking_loss = label_ranking_loss(test_label, scores)
-print "Ranking loss: " + str(ranking_loss)
+# ranking_losses.append(ranking_loss)
+print "DecisionTree - Ranking loss: " + str(ranking_loss)
+
+# plt.plot(range(9,31), ranking_losses)
+# plt.show()
